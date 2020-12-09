0245f016d9ed6fce7bf6c75a89843d92e9595088
yuta <ogutoogumogura@gmail.com>
2020-12-09 12:11:44 +0900

    とりあえずなんか鳴る

--- src/renderer/components/EachStep.vue
+++ src/renderer/components/EachStep.vue
@@ -10,6 +10,9 @@
     <v-btn icon color="green" @click="clearSlide"
       ><v-icon>mdi-trash-can</v-icon></v-btn
     >
+    <v-btn icon color="green" @click="playSound"
+      ><v-icon>mdi-volume-high</v-icon></v-btn
+    >
     <swiper ref="mySwiper" :options="swiperOption">
       <img-item
         v-for="card in cards"
@@ -26,6 +29,7 @@
 
 <script>
 import ImageCard from "@/components/ImageCard.vue";
+const Tone = require('tone');
 const {spawnSync,execSync} = require("child_process");
 
 export default {
@@ -121,6 +125,32 @@ export default {
       this.cards.splice(0);
       this.count = 0;
     },
+    //setplay: function(time,note){synth.triggerAttackRelease(note,'4n',time);},
+    playSound: function(){
+      var Dm7 = ['D3', 'F4', 'A4', 'C5', 'E5'];
+      var G7 = ['G3', 'F4','A4','B4','D5'];
+      var CM7 = ['C3', 'E4', 'G4','B4','D5'];
+      var A7 = ['A3', 'Db4','E4','G4', 'A4']
+      const sequence =[['0:0:0', Dm7],
+                      ['0:1:0', G7],
+                      ['0:2:0', CM7],
+                      ['0:3:0',A7]];
+
+
+      var synth = new Tone.PolySynth().toDestination();
+      function setplay(time,note){synth.triggerAttackRelease(note,'4n',time);};
+      const melody = new Tone.Part(setplay,sequence);
+      // melody.stop();
+      // melody.start();
+      //ループ回数
+      melody.loop = 0; 
+      //テンポ
+      Tone.Transport.bpm.value = 60;
+      //再生実行
+      Tone.Transport.cancel()
+      melody.start();
+      Tone.Transport.start(); 
+    }
   },
 };
 </script>
